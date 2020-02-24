5437140027997e90186023409a23be8c68008a25
Jonathan <JonathanMac@Jonathans-MacBook-Air.local>
2020-02-23 21:23:08 -0500

    Webscrapes FnG Index and stores the data in a .csv file.

---
+++ fng.py
@@ -0,0 +1,28 @@
+import requests, bs4
+import csv
+import datetime
+
+date_x = datetime.datetime.now()
+date = (date_x.strftime("%x"))
+
+res = requests.get('https://alternative.me/crypto/fear-and-greed-index/#fng-history')
+res.raise_for_status()
+
+fngSoup = bs4.BeautifulSoup(res.text, features = "lxml")
+
+csv_file = open('fng_data.csv', 'a')
+
+csv_writer = csv.writer(csv_file)
+
+
+fngSearch = fngSoup.findAll('div', {"class": "fng-circle"})
+
+line = fngSearch[0]
+fng = line.getText()
+
+print(fng)
+
+print()
+
+csv_writer.writerow([])
+csv_writer.writerow([date, fng])
